d785ad12f7e77ff96db26e484d979d167d171999
Grant Chatterton <grantchatterton@gmail.com>
2025-12-02 00:00:08 -0600

    chore: add ESLint configuration and scripts for linting files (#13)
    - Added lint and lint:fix scripts to package.json for ESLint
    - Updated devDependencies to include ESLint and related packages

---
+++ eslint.config.ts
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    ignores: ['node_modules/**', 'dist/**', 'build/**'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+]);
